8238578ee4eb661495ccd66496df3b619d672865
Mason Embry <mason@embrycode.com>
2016-12-10 16:43:36 -0500

    Carousels working.

--- js/custom.js
+++ js/custom.js
@@ -58,23 +58,18 @@ jQuery(function($) {
   });
 
   $('.gallery-carousel').slick({
-    centerMode: true,
-    centerPadding: '60px',
     slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [{
       breakpoint: 768,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+        slidesToShow: 2
       }
     }, {
       breakpoint: 480,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
         slidesToShow: 1
       }
     }]
